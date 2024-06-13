7544cdc09c8b2a3a83c295d060e09950b2e377b0
JamesTTTT <taylormarkaryd@gmail.com>
2024-06-13 23:10:05 +0200

    Added output

--- react-frontend/src/components/api_testing/SendRequestButton.jsx
+++ react-frontend/src/components/api_testing/SendRequestButton.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useRequest } from "../../contexts/RequestContext";
+import { requestHandler } from "../../models/requestHandler";
 
 const SendRequestButton = () => {
+  const { getRequestData, setOutput } = useRequest();
+
   return (
     <button
       className="px-3 bg-primary text-neutral text-xs py-[10px] 
     font-bold border border-l-0 hover:opacity-75"
+      onClick={async () => {
+        const res = await requestHandler.request(getRequestData);
+        setOutput(res);
+      }}
     >
       SEND
     </button>
