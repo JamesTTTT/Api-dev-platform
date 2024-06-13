82d3b0e70956deb447cdb37daa858944a77d3e66
JamesTTTT <taylormarkaryd@gmail.com>
2024-06-13 23:05:21 +0200

    Added respononse window

--- react-frontend/src/components/api_testing/ResponseDisplay.jsx
+++ react-frontend/src/components/api_testing/ResponseDisplay.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useRequest } from "../../contexts/RequestContext";
+
+const PrettyJsonComponent = ({ jsonString }) => {
+  const prettyJson = JSON.stringify(JSON.parse(jsonString), null, 2);
+
+  return <pre className="p-4 hitespace-pre-wrap break-words">{prettyJson}</pre>;
+};
 
 const ResponseDisplay = () => {
-  return <div>ResponseDisplay</div>;
+  const { output } = useRequest();
+  return (
+    <div>
+      <PrettyJsonComponent jsonString={output.body} />
+    </div>
+  );
 };
 
 export default ResponseDisplay;
