63fccabd721fac07c250935df43d683f5eabb965
JamesTTTT <taylormarkaryd@gmail.com>
2024-06-13 23:15:41 +0200

    Added context tracking for my request context

--- react-frontend/src/components/api_testing/UrlBar.jsx
+++ react-frontend/src/components/api_testing/UrlBar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useRequest } from "../../contexts/RequestContext";
 
 const UrlBar = () => {
+  const { getRequestData, setURL } = useRequest();
+
   return (
     <div className="w-full">
       <input
+        value={getRequestData.url}
+        onChange={(e) => {
+          setURL(e.target.value);
+        }}
         className="px-2 py-[10px] w-full outline-none border-y text-xs bg-transparent"
         placeholder="Enter URL"
       />
