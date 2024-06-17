9cb31a5e5248e8a2a7bbcabdfbd436ea587b2c16
JamesTTTT <taylormarkaryd@gmail.com>
2024-06-17 22:05:02 +0200

    New styling

--- react-frontend/src/components/api_testing/UrlBar.jsx
+++ react-frontend/src/components/api_testing/UrlBar.jsx
@@ -5,13 +5,13 @@ const UrlBar = () => {
   const { getRequestData, setURL } = useRequest();
 
   return (
-    <div className="w-full">
+    <div className="w-full flex align-middle">
       <input
         value={getRequestData.url}
         onChange={(e) => {
           setURL(e.target.value);
         }}
-        className="px-2 py-[10px] w-full outline-none border-y text-xs bg-transparent"
+        className="px-2 py-[10px] w-full outline-none text-xs bg-transparent"
         placeholder="Enter URL"
       />
     </div>
