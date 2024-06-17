400d7a1cee365aacc771ef069ba6e077a67b0c01
JamesTTTT <taylormarkaryd@gmail.com>
2024-06-17 22:04:22 +0200

    export new components

--- react-frontend/src/components/index.js
+++ react-frontend/src/components/index.js
@@ -3,3 +3,8 @@ export { default as SendRequestButton } from "./api_testing/SendRequestButton";
 export { default as MethodSelector } from "./api_testing/MethodSelector";
 export { default as ResponseDisplay } from "./api_testing/ResponseDisplay";
 export { default as RequestBody } from "./api_testing/RequestBody";
+export { default as RequestHeaders } from "./api_testing/RequestHeaders";
+
+export { default as ResponseHeaders } from "./api_testing/ResponseHeaders";
+
+export { default as CodeEditor } from "./essentials/CodeEditor";
